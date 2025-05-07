6fd75aed6c2777a2e67ae1834e3e484ca1670d36
pomppumatto <aaponurmi01@gmail.com>
2025-05-07 20:32:44 +0300

    Beginning of code

--- Password_manager.py
+++ Password_manager.py
@@ -1,3 +1,25 @@
-def main():
-    length = int(input("How many characters do you want to generate?: "))
-    print(f"pituus on: {length}")
+import random
+import string
+
+s1 = list(string.punctuation)
+s2 = list(string.digits)
+s3 = list(string.ascii_uppercase)
+s4 = list(string.ascii_lowercase)
+
+full_amount = int(input("How many password do we generate: "))
+amount = 0
+
+while True:
+
+    try:
+        length = int(input("How many characters do you want for your password?: "))
+        
+        if length < 8:
+            print("Minimum amount of characters is 8")
+
+        else:
+            break
+
+    except:
+        print("please numbers only")
+        continue
